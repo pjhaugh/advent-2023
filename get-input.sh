947ffb54b80c18c03d93d0def3a72a426e95d2a4
Patrick Haugh <haughpj@gmail.com>
2023-11-27 20:23:07 -0500

    Initial

---
+++ get-input.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+set -e
+
+OUTDIR="$(dirname "${(%):-%N}")/inputs"
+
+YEAR=2023
+DAY=$1
+
+curl -b session="$(cat ~/.aocsession)" "https://adventofcode.com/$YEAR/day/$DAY/input" > "$OUTDIR/input-$(printf %02d "$DAY")-$YEAR.txt"
